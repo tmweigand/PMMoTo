9205369dcc647d6fa7863b62d618fe46bbffd2d8
Timothy Weigand <68024672+tmweigand@users.noreply.github.com>
2024-07-19 21:24:28 -0400

    cleans naming with type hints

--- pmmoto/core/domain_discretization.py
+++ pmmoto/core/domain_discretization.py
@@ -9,17 +9,20 @@ class DiscretizedDomain(pmmoto_domain.Domain):
     Class for discretizing the domain
     """
 
-    def __init__(self, num_voxels: tuple[int, ...] = (1, 1, 1), **kwargs):
+    def __init__(self, num_voxels: tuple[int,int,int] = (1, 1, 1), **kwargs):
         super().__init__(**kwargs)
         self.num_voxels = num_voxels
-        self.voxel = self.get_voxel_size()
+        self.res = self.get_voxel_size()
 
     def get_voxel_size(self) -> tuple[float,...]:
         """
-        Get domain length and voxel size
+        Get domain length and resolution
         """
-        voxel = np.zeros([self.dims])
+        resolution = np.zeros([self.dims],dtype=float)
         for n in range(0, self.dims):
-            voxel[n] = self.length_domain[n] / self.num_voxels[n]
+            if self.num_voxels[n] < 1:
+                print('Error')
+            else:
+                resolution[n] = self.length[n] / self.num_voxels[n]
 
-        return tuple(voxel)
+        return tuple(resolution)
---
+++ tests/core/test_domain_decomposed.py
@@ -0,0 +1,36 @@
+import pytest
+import numpy as np
+import pmmoto
+
+def test_decomposed_domain(
+        domain,
+        domain_discretization,
+        decomposed_domain
+        ):
+    """
+    Test decomposition of domain  
+    """
+
+    pmmoto_decomposed_domain = pmmoto.core.DecomposedDomain(
+        num_voxels = domain_discretization['num_voxels'],
+        box = domain['box'],
+        boundaries = domain['boundaries'],
+        inlet = domain['inlet'],
+        outlet = domain['outlet'],
+        subdomain_map = decomposed_domain['subdomain_map']
+        )
+    
+    pmmoto_index = pmmoto_decomposed_domain.get_subdomain_index(0)
+    boundaries = pmmoto_decomposed_domain.get_subdomain_boundaries(pmmoto_index)
+    # box = pmmoto_decomposed_domain.get_subdomain_box(pmmoto_index,)
+    inlet = pmmoto_decomposed_domain.get_subdomain_inlet(pmmoto_index)
+    outlet = pmmoto_decomposed_domain.get_subdomain_outlet(pmmoto_index)
+    
+    np.testing.assert_array_equal(pmmoto_decomposed_domain.map.flatten(),np.arange(pmmoto_decomposed_domain.num_subdomains))
+    # assert len(pmmoto_decomposed_domain.subdomains) == pmmoto_decomposed_domain.num_subdomains
+
+    assert pmmoto_index == (0,0,0)
+    np.testing.assert_array_equal(boundaries, np.array([[0,-1],[1,-1],[2,-1]]))
+    # np.testing.assert_array_equal(box, np.array([[0.,50.],[0.,50.],[0.,50.]]))
+    np.testing.assert_array_equal(inlet, np.array([[1,0],[0,0],[0,0]]))
+    np.testing.assert_array_equal(outlet, np.array([[0,0],[0,0],[0,0]]))
