e1293480dbec85e9dad1eff5d0d457c10d874da0
kbruning <kbruning@live.unc.edu>
2024-08-17 14:46:09 -0400

    Add morph test case

---
+++ tests/submit_pack1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=PMMoTo
+#SBATCH -N 1
+#SBATCH -p debug_queue
+#SBATCH -n 8
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=00:30:00
+
+module load openmpi_4.0.1/gcc_11.2.0
+module load python/3.10.14
+
+date
+
+mpirun -np 8 python3 testOpen.py
+
+date
