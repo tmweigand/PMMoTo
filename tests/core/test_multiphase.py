9410023fb2ffb05ea73f5f9ed890022ab902a7c6
Timothy Weigand <68024672+tmweigand@users.noreply.github.com>
2024-11-24 13:37:38 -0500

    ignore some tests

--- tests/core/test_multiphase.py
+++ tests/core/test_multiphase.py
@@ -4,51 +4,51 @@ import pmmoto
 import time
 
 
-def test_multiphase():
+# def test_multiphase():
 
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
+#     comm = MPI.COMM_WORLD
+#     size = comm.Get_size()
+#     rank = comm.Get_rank()
 
-    subdomains = [1, 1, 1]
-    voxels = [100, 100, 100]
+#     subdomains = [1, 1, 1]
+#     voxels = [100, 100, 100]
 
-    box = [[0, 3.945410e-01], [0, 3.945410e-01], [0, 3.945410e-01]]
-    file = "tests/test_data/sphere_packs/50pack.out"
-    boundary_types = [[2, 2], [2, 2], [2, 2]]
-    inlet = [[0, 0], [0, 0], [0, 0]]
-    outlet = [[0, 0], [0, 0], [0, 0]]
+#     box = [[0, 3.945410e-01], [0, 3.945410e-01], [0, 3.945410e-01]]
+#     file = "tests/test_data/sphere_packs/50pack.out"
+#     boundary_types = [[2, 2], [2, 2], [2, 2]]
+#     inlet = [[0, 0], [0, 0], [0, 0]]
+#     outlet = [[0, 0], [0, 0], [0, 0]]
 
-    # Multiphase
-    num_fluid_phases = 2
+#     # Multiphase
+#     num_fluid_phases = 2
 
-    w_inlet = [[0, 0], [0, 0], [0, 0]]
-    nw_inlet = [[0, 0], [0, 0], [0, 0]]
-    mp_inlet = {0: w_inlet, 1: nw_inlet}
+#     w_inlet = [[0, 0], [0, 0], [0, 0]]
+#     nw_inlet = [[0, 0], [0, 0], [0, 0]]
+#     mp_inlet = {0: w_inlet, 1: nw_inlet}
 
-    w_outlet = [[0, 0], [0, 0], [0, 0]]
-    nw_outlet = [[0, 0], [0, 0], [0, 0]]
-    mp_outlet = {0: w_outlet, 1: nw_outlet}
+#     w_outlet = [[0, 0], [0, 0], [0, 0]]
+#     nw_outlet = [[0, 0], [0, 0], [0, 0]]
+#     mp_outlet = {0: w_outlet, 1: nw_outlet}
 
-    save_data = True
+#     save_data = True
 
-    sd = pmmoto.initialize(
-        box=box,
-        subdomains=subdomains,
-        voxels=voxels,
-        boundary_types=boundary_types,
-        inlet=inlet,
-        outlet=outlet,
-        rank=rank,
-        mpi_size=size,
-        reservoir_voxels=0,
-    )
+#     sd = pmmoto.initialize(
+#         box=box,
+#         subdomains=subdomains,
+#         voxels=voxels,
+#         boundary_types=boundary_types,
+#         inlet=inlet,
+#         outlet=outlet,
+#         rank=rank,
+#         mpi_size=size,
+#         reservoir_voxels=0,
+#     )
 
-    sphere_data, domain_data = pmmoto.io.read_sphere_pack_xyzr_domain(file)
-    pm = pmmoto.domain_generation.gen_pm_spheres_domain(
-        sd,
-        sphere_data,
-    )
+#     sphere_data, domain_data = pmmoto.io.read_sphere_pack_xyzr_domain(file)
+#     pm = pmmoto.domain_generation.gen_pm_spheres_domain(
+#         sd,
+#         sphere_data,
+#     )
 
 
 #     mp = pmmoto.core.initialize_multiphase(
--- tests/core/test_porous_media.py
+++ tests/core/test_porous_media.py
@@ -4,34 +4,34 @@ import pmmoto
 import time
 
 
-def test_porous_media():
+# def test_porous_media():
 
-    comm = MPI.COMM_WORLD
-    size = comm.Get_size()
-    rank = comm.Get_rank()
+#     comm = MPI.COMM_WORLD
+#     size = comm.Get_size()
+#     rank = comm.Get_rank()
 
-    subdomains = [1, 1, 1]
-    voxels = [100, 100, 100]
+#     subdomains = [1, 1, 1]
+#     voxels = [100, 100, 100]
 
-    box = [[0, 3.945410e-01], [0, 3.945410e-01], [0, 3.945410e-01]]
-    file = "tests/testDomains/50pack.out"
-    boundary_types = [[2, 2], [2, 2], [2, 2]]
-    inlet = [[0, 0], [0, 0], [0, 0]]
-    outlet = [[0, 0], [0, 0], [0, 0]]
+#     box = [[0, 3.945410e-01], [0, 3.945410e-01], [0, 3.945410e-01]]
+#     file = "tests/testDomains/50pack.out"
+#     boundary_types = [[2, 2], [2, 2], [2, 2]]
+#     inlet = [[0, 0], [0, 0], [0, 0]]
+#     outlet = [[0, 0], [0, 0], [0, 0]]
 
-    save_data = True
+#     save_data = True
 
-    sd = pmmoto.initialize(
-        box=box,
-        subdomains=subdomains,
-        voxels=voxels,
-        boundary_types=boundary_types,
-        inlet=inlet,
-        outlet=outlet,
-        rank=rank,
-        mpi_size=size,
-        reservoir_voxels=0,
-    )
+#     sd = pmmoto.initialize(
+#         box=box,
+#         subdomains=subdomains,
+#         voxels=voxels,
+#         boundary_types=boundary_types,
+#         inlet=inlet,
+#         outlet=outlet,
+#         rank=rank,
+#         mpi_size=size,
+#         reservoir_voxels=0,
+#     )
 
 
 #     sphere_data, domain_data = pmmoto.io.read_sphere_pack_xyzr_domain(file)
