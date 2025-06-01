9f8a6b8ce1e4f13801bcd79f491c20ec0ba671eb
Timothy Weigand <68024672+tmweigand@users.noreply.github.com>
2025-05-31 23:36:16 -0400

    figuring out website again agin

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-sys.path.insert(0, os.path.abspath("src"))  # Adjust if different
+sys.path.insert(0, os.path.abspath("../src"))  # Adjust if different
 
 extensions = [
     "sphinx.ext.autodoc",
