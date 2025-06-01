58b2890c5718de9f962faaec56d134ea71701c71
Timothy Weigand <68024672+tmweigand@users.noreply.github.com>
2025-05-31 20:36:54 -0400

    removes old sets

--- src/pmmoto/core/__init__.py
+++ src/pmmoto/core/__init__.py
@@ -3,8 +3,6 @@ from . import communication
 from . import domain
 from . import domain_decompose
 from . import domain_discretization
-
-# from . import sets
 from . import subdomain
 from . import subdomain_padded
 from . import subdomain_features
